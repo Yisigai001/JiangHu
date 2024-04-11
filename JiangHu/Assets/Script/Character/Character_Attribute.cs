27859fae892c1b6254824d4e93a212ac2e98ef83
guan.wang <184680001@qq.com>
2024-04-11 14:24:46 +0800

    上传

--- JiangHu/Assets/Script/Character/Character_Attribute.cs
+++ JiangHu/Assets/Script/Character/Character_Attribute.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class Character_Attribute : MonoBehaviour
 {
+    [Header("基础属性")]
+    public int CharacterID; //角色id
+    public int camp; //阵营
+    public int genGu; //根骨
+    public int jinLi; //劲力
+    public int lingQiao; //灵巧
+    public int yang; //阳
+    public int yin; //阴
+    public int neiLi;
+    public int xinZhi;
+    public int wuXing;
+    public int fuYuan;
+
+    [Header("系统成长")]
+    public int jingMai;
+    public int duanTi;
+
+    [Header("战斗属性")]
+    public int gangGong; //
+    public int rouGong;
+    public int yangGong;
+    public int yinGong;
+    public int taiJi;
+    public int gangMian;
+    public int rouMian;
+    public int yangMian;
+    public int yinMian;
+
+    public int gangGongBai;
+    public int rouGongBai;
+    public int yangGongBai;
+    public int yinGongBai;
 
     // Start is called before the first frame update
     void Start()
@@ -11,9 +43,8 @@ public class Character_Attribute : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetAttribute()
     {
-        
+
     }
 }
